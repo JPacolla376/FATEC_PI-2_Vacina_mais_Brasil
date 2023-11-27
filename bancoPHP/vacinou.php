47019f378c950d3577575bb2770cac582d6cd3f3
William-Fonseca-Geralde <william.geralde@fatec.sp.gov.br>
2023-11-27 20:27:05 -0300

    commit

--- bancoPHP/vacinou.php
+++ bancoPHP/vacinou.php
@@ -3,31 +3,30 @@
 include_once("conexao.php");
 
 class Vacinou extends Conectar {
-  private $data_vacinado, $conec, $id_vacina, $cpf_usr;
 
   public function inserirVacinaUsuario($cpf_usr, $id_vacina, $data_vacinado) {
     try {
       $sql = "SELECT * FROM usuario WHERE CPF = $cpf_usr";
-      $stmt_usr = $this->conec->getConn()->query($sql)->fetch();
+      $stmt_usr = $this->getConn()->query($sql)->fetch();
     } catch (PDOException $e) {
       echo $sql . "<br>" . $e->getMessage();
     }
 
     try {
       $sql = "SELECT * FROM vacinas WHERE ID = $id_vacina";
-      $stmt_vac = $this->conec->getConn()->query($sql)->fetch();
+      $stmt_vac = $this->getConn()->query($sql)->fetch();
     } catch (PDOException $e) {
       echo $sql . "<br>" . $e->getMessage();
     }
 
     if ($stmt_usr != false && $stmt_vac != false) {
-      $this->data_vacinado = $data_vacinado;
-      $this->cpf_usr = $cpf_usr;
-      $this->id_vacina = $id_vacina;
-
       try {
-        $sql = "INSERT INTO vacinou (fk_Usuario_CPF, fk_Vacinas_ID, Data_Vacinado) VALUES ($cpf_usr, $id_vacina, '$data_vacinado')";
-        $stmt = $this->conec->getConn()->query($sql);
+        $sql = "INSERT INTO vacinou (fk_Usuario_CPF, fk_Vacinas_ID, Data_Vacinado) VALUES (:cpf_usr, :id_vacina, ':data_vacinado')";
+        $stmt = $this->getConn()->prepare($sql);
+        $stmt->bindParam(':cpf_usr', $cpf_usr);
+        $stmt->bindParam(':id_vacina', $id_vacina);
+        $stmt->bindParam(':data_vacinado', $data_vacinado);
+        $stmt->execute();
       } catch (PDOException $e) {
         echo $sql . "<br>" . $e->getMessage();
       }
