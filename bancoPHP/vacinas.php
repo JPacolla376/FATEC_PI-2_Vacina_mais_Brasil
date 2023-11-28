9d468202815da5f842428d586de41d4d8d9b5319
William-Fonseca-Geralde <william.geralde@fatec.sp.gov.br>
2023-11-28 19:25:12 -0300

    commit

--- bancoPHP/vacinas.php
+++ bancoPHP/vacinas.php
@@ -3,18 +3,17 @@
 include_once("conexao.php");
 
 class Vacinas extends Conectar {
-  private $id, $nome, $origem, $data_validade, $data_fabricacao, $conec;
 
   public function inserirVacina($id, $nome, $origem, $data_validade, $data_fabricacao) {
-    $this->id = $id;
-    $this->nome = $nome;
-    $this->origem = $origem;
-    $this->data_validade = $data_validade;
-    $this->data_fabricacao =$data_fabricacao;
-
     try {
-      $sql = "INSERT INTO vacinas (ID, Nome, Origem, Data_Validade, Data_Fabricacao) VALUES ($id, '$nome', '$origem', '$data_validade', '$data_fabricacao')";
-      $stmt = $this->conec->getConn()->query($sql);
+      $sql = "INSERT INTO vacinas (ID, Nome, Origem, Data_Validade, Data_Fabricacao) VALUES (:id, ':nome', ':origem', ':data_validade', ':data_fabricacao')";
+      $stmt = $this->getConn()->prepare($sql);
+      $stmt->bindParam(':id', $id);
+      $stmt->bindParam(':nome', $nome);
+      $stmt->bindParam(':origem', $origem);
+      $stmt->bindParam(':da$data_validade', $data_validade);
+      $stmt->bindParam(':da$data_fabricacao',$data_fabricacao);
+      $stmt->execute();
     } catch (PDOException $e) {
       echo $sql . "<br>" . $e->getMessage();
     }
