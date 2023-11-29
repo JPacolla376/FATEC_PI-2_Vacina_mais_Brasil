5c83dac938db0f25c28ba150fc331baa51bb4a15
William-Fonseca-Geralde <william.geralde@fatec.sp.gov.br>
2023-11-28 21:50:53 -0300

    mostrar as vacinas

--- bancoPHP/vacinou.php
+++ bancoPHP/vacinou.php
@@ -34,6 +34,30 @@ class Vacinou extends Conectar {
       echo "ID da vacina ou o CPF do usuário está errado";
     }
   }
+
+  public function MostrarVacinasTomadas() {
+    if ($_SESSION['status'] == true) {
+      try {
+        $usuario = $_SESSION['usuario'];
+        $sql = "SELECT * FROM usuario u INNER JOIN vacinou vu INNER JOIN vacinas v ON u.CPF = vu.fk_Usuario_CPF AND v.ID = vu.fk_Vacinas_ID WHERE u.Nome = '$usuario'";
+        $stmt = $this->getConn()->query($sql);
+        if ($stmt->rowCount() > 0) {
+          while ($row = $stmt->fetch()) {
+            echo "<div class = 'card'>" .
+              "<h3>" . $row["Nome"] . "</h3>" . 
+              "<h4> Origem: " . $row["Origem"] . "</h4>" .
+              "<h4> Data Vacinado: " . $row["Data_Vacinado"] . "</h4>";
+          }
+        } else {
+          echo "Não tem vacinas";
+        }
+      } catch (PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
+      }
+    } else {
+      echo "É necessário logar no site";
+    }
+  }
 }
 
 ?>
